a15095bd1059a7e3067b8b7e41a3abae60946744
Martin Kwok <mjkwok@google.com>
2021-12-11 14:35:37 -0800

    Formatting.

--- keyboards/planck/keymaps/synook/keymap.c
+++ keyboards/planck/keymaps/synook/keymap.c
@@ -1,22 +1,5 @@
-/* Copyright 2015-2021 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 #include "muse.h"
-
 
 enum planck_layers {
   _COLEMAK,
@@ -78,10 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAMERAISE] = LAYOUT_planck_grid(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
     _______, KC_GRV,  KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, _______, _______, KC_ENT,
-    _______, KC_P,    KC_COMM, KC_DOT,  KC_SLSH, KC_F11,  KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END, COLEMAK,
+    _______, KC_P,    KC_COMM, KC_DOT,  KC_SLSH, KC_F11,  KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  COLEMAK,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
-
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
